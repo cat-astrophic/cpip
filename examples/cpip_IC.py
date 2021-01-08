d78035db93c41660a9c680753baf2d0055120627
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2021-01-08 16:29:58 -0500

    applies cpip to data on international collaborations

--- examples/cpip_IC.py
+++ examples/cpip_IC.py
@@ -5,7 +5,8 @@
 # Importing required modules
 
 import pandas as pd
-import numpy as np
+import geopandas as gpd
+from matplotlib import pyplot as plt
 import networkx as nx
 from cpip import *
 
@@ -32,12 +33,29 @@ for f in files:
     print(core3)
     print(core4)
 
-# Plotting this network on a world political map
+# Plotting Southern African part of this network on a world political map for year 2019
 
-data = pd.read_csv(data_path + files[-1]) # reading in the data
-
-
-
-# plot the core on a world political map??
-
+cm = plt.get_cmap('tab20')
+plt.figure()
+data = pd.read_csv(data_path + files[-1])
+sa = ['Angola', 'Zambia', 'Malawi', 'Mozambique', 'Namibia', 'Botswana',
+      'Zimbabwe', 'South Africa', 'eSwatini', 'Lesotho']
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+world = world[world.name.isin(sa)]
+base = world.plot(color = 'white', edgecolor = 'black')
+sa = list(sorted(['Eswatini' if s == 'eSwatini' else s for s in sa]))
+idxset = [list(data.columns).index(s) for s in sa]
+data = data[sa]
+data = data[data.index.isin(idxset)]
+rwidths = [data.values[i][j] for i in range(len(data)) for j in range(i,len(data))]
+rwidths = [5 * width / max(rwidths) for width in rwidths]
+rnodes = [sum(list(data[c])) for c in data.columns]
+rg = nx.MultiGraph([(i,j) for i in range(len(data)) for j in range(i,len(data))])
+latlon = [[-12.4,17.4], [-23.9,22.7], [-26.4,31.6], [-29.6,28.1], [-13,33.8],
+          [-18,35], [-21,16.5], [-31.1,21.1], [-13.9,26.5], [-17.8,30]]
+rpos = [[coord[1],coord[0]] for coord in latlon]
+nx.draw(rg, rpos, width = rwidths, node_color = cm(2/3))
+plt.title('International Collaborations\nAcross Southern Africa', loc = 'center', fontsize = 20, color = 'black')
+plt.savefig('C:/Users/User/Documents/Data/cpip_tests/collaborations_example.eps')
+plt.show()
 
